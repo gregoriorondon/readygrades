b4e4f065ff09bee089c69620bf9388af7d4964e5
gregoriorondon <gregorioalexanderondon@gmail.com>
2023-12-19 16:39:53 -0400

    icon index
    se agrega los iconos al footer en la seccion de social de index.php

--- elements/footer.php
+++ elements/footer.php
@@ -5,8 +5,8 @@
         <li><a href="#">Contacto</a></li>
     </ul>
     <ul class="f-social">
-        <li><a href="https://upttmbi.edu.ve/site/index.html" target="_blank">Web Oficial</a></li>
-        <li><a href="https://www.instagram.com/upttmbi/" target="_blank">Instagram</a></li>
-        <li><a href="https://www.twitter.com/upttmbi" target="_blank">Twitter</a></li>
+        <li><a href="https://upttmbi.edu.ve/site/index.html" target="_blank"><i class="fa-solid fa-globe"></i>Web Oficial</a></li>
+        <li><a href="https://www.instagram.com/upttmbi/" target="_blank"><i class="fa-brands fa-instagram"></i>Instagram</a></li>
+        <li><a href="https://www.twitter.com/upttmbi" target="_blank"><i class="fa-brands fa-x-twitter"></i>Twitter</a></li>
     </ul>
 </footer>
