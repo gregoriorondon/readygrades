001c60b6607ddea078e5b8a9a9940c9b2a15ff45
gregoriorondon <gregorioalexanderondon@gmail.com>
2023-12-18 20:26:33 -0400

    reaordenado
    creacion de la carpeta css para la lectura correcta de los elementos externos de php

--- elements/footer.php
+++ elements/footer.php
@@ -1,7 +1,20 @@
-<footer>
+<!DOCTYPE html>
+<html lang="es">
+<head> <link rel="stylesheet" href="css/estilos.css"></head>
+<body>
+    
+<footer class="info">
     <ul class="f-info">
         <li><a href="#">Sobre Nosotros</a></li>
         <li><a href="#">Ubicación</a></li>
         <li><a href="#">Contacto</a></li>
     </ul>
+    <ul class="f-social">
+        <li><a href="https://upttmbi.edu.ve/site/index.html">Web Oficial</a></li>
+        <li><a href="https://www.instagram.com/upttmbi/">Instagram</a></li>
+        <li><a href="https://www.twitter.com/upttmbi">Twitter</a></li>
+    </ul>
 </footer>
+
+</body>
+</html>
