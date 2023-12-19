6adc54f8fa171c8f5f7b44fad5ce862413464c47
gregoriorondon <gregorioalexanderondon@gmail.com>
2023-12-18 21:07:39 -0400

    simplificacion
    simplificacion del codigo del elemento footer y de los estilos de este junto con una mejora en el los margenes del body del sitio principal

--- elements/footer.php
+++ elements/footer.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head> <link rel="stylesheet" href="css/estilos.css"></head>
-<body>
-    
 <footer class="info">
     <ul class="f-info">
         <li><a href="#">Sobre Nosotros</a></li>
@@ -10,11 +5,8 @@
         <li><a href="#">Contacto</a></li>
     </ul>
     <ul class="f-social">
-        <li><a href="https://upttmbi.edu.ve/site/index.html">Web Oficial</a></li>
-        <li><a href="https://www.instagram.com/upttmbi/">Instagram</a></li>
-        <li><a href="https://www.twitter.com/upttmbi">Twitter</a></li>
+        <li><a href="https://upttmbi.edu.ve/site/index.html" target="_blank">Web Oficial</a></li>
+        <li><a href="https://www.instagram.com/upttmbi/" target="_blank">Instagram</a></li>
+        <li><a href="https://www.twitter.com/upttmbi" target="_blank">Twitter</a></li>
     </ul>
 </footer>
-
-</body>
-</html>
