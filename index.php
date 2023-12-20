deb28f9665c8ea41c5c395ddbd8798d13750b13c
gregoriorondon <gregorioalexanderondon@gmail.com>
2023-12-20 11:22:42 -0400

    img
    se inserto una ilustracion al lado de sobre nosotros ademas se le agrego un icono junto con el efecto hover al boton de leer mas

--- index.php
+++ index.php
@@ -19,15 +19,16 @@
     </center>
 </div>
 <section class="sec-about">
+    <div class="i-about">
+        <img class="img-about" src="./img/Classroom-rafiki.png" alt="https://storyset.com/education"><a href="https://storyset.com/education" target="_blank">Education illustrations by Storyset</a>
+    </div>
     <div class="s-about">
         <h1>Sobre Nosotros</h1>
-        <p>La universidad Politécnica Territorial de Estado Trujillo “Mario Briceño Iragorry”, 
+        <p>La Universidad Politécnica Territorial de Estado Trujillo “Mario Briceño Iragorry”, 
 es una institución enmarcada dentro de la Educación Media General y de...</p>
         <a href="./about/about.php">Leer más<i class="fa-solid fa-arrow-right"></i></a>
     </div>
-    <div class="i-about">
-        <img src="" alt="">
-    </div>
+
 </section>
 <?php
 include './elements/footer.php'
