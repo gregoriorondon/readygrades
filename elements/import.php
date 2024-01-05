e722a582d021f09392a2960350b01f28ced1eb6d
gregoriorondon <gregorioalexanderondon@gmail.com>
2024-01-05 12:37:51 -0400

    mejoras css login14
    se mejoro el ingreso para los estudiantes de forma visual ademas se mejoro el elemento import de css para que no cargue doblemente los estilos tambien se agrego la transicion o hover en los botones de estudiante y profesor

--- elements/import.php
+++ elements/import.php
@@ -1,5 +1,3 @@
     <link rel="stylesheet" href="./css/estilos.css">
-    <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="./fonawesome/fontawesome-free-6.5.1-web/css/all.min.css">
-    <link rel="stylesheet" href="fonawesome/fontawesome-free-6.5.1-web/css/all.min.css">
     <link rel="shortcut icon" href="./img/logouptt.png" type="image/x-icon">
--- estudiantes.php
+++ estudiantes.php
@@ -3,10 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Estudiantes</title>
+    <?php include './elements/import.php'?>
 </head>
 <body>
-    <input type="number" name="" id="" placeholder="Inserte su Cedula de Identidad">
-    <input type="submit" value="Ir">
+<?php include './elements/nav.php'?>
+
+<form action="">
+
+<div class="stud">
+    <center class="log1">
+        <img src="./img/logosystem.png" alt="">
+        <img src="./img/logouptt.png" alt="">
+    </center>
+    <div class="log">
+        <input type="number" name="" id="" placeholder="Inserte su Cedula de Identidad">
+        <button type="submit">Enviar</button>
+    </div>
+    <div class="warni">
+        <span class="war1">Tenga en cuenta que si intenta copiar o tomar alguna foto de las notas que quiere visualizar</span>
+        <span class="war2">NO TIENEN NINGÚN VALOR ACADÉMICO</span>
+    </div>
+</div>
+</form>
+<div class="minifoot">
+    <span>SISTEMA DESARROLLADO POR GREGORIO RONDÓN, CARLOS RAMOS Y YANITZA BARRIGA</span>
+</div>
+
 </body>
 </html>
