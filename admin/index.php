18ba2fe07189fdd97e476762db9eaae2e3f35496
gregoriorondon <gregorioalexanderondon@gmail.com>
2024-01-09 15:57:20 -0400

    se agregaron estilos admin

--- admin/index.php
+++ admin/index.php
@@ -6,17 +6,18 @@
     <title>Sección Administrativo</title>
     <?php include './elements-admin/import.php'; include './elements-admin/warning.php'?>
 </head>
-<body>
+<body class="warning">
 <script>
    // alert('Esta Es Una Sección Solo Para Personal Autorizado');
 </script>
 
 <center>
-    <h1 class="warni-icon"><i class="fa-solid fa-radiation"></i></h1>
-    <h1>Esta Entrando En La Sección De Administradores</h1>
-    <h2>ESTA ES SOLO UNA SECCIÓN PARA PERSONAL AUTORIZADO <span class="advert">Si Usted No Está Autorizado Salga De Inmediato</span> </h2>
+    <h1 class="warni-icon"><i class="fa-solid fa-circle-radiation"></i></h1>
+    <h1 class="warni-text">ADVERTENCIA</h1>
+    <h1>Estas Entrando En La Sección De Administradores</h1>
+    <h2>ÉSTA ES SOLO UNA SECCIÓN PARA PERSONAL AUTORIZADO <br><span class="advert-d"> Si Usted No Está Autorizado </span><span class="advert">Salga De Inmediato</span> </h2>
 
-    <button>Soy Administrador</button>
+    <button class="sub">Soy Administrador</button>
 
 </center>
 
