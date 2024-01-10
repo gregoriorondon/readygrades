9d55b7b714ffecd4b86c24ba56f094e28519291c
gregoriorondon <gregorioalexanderondon@gmail.com>
2024-01-10 17:46:10 -0400

    se mejoro el login

--- admin/elements-admin/minifoot.php
+++ admin/elements-admin/minifoot.php
@@ -1,4 +1,6 @@
 <div class="minifoot">
     <span>SISTEMA DESARROLLADO POR GREGORIO RONDÓN, CARLOS RAMOS Y YANITZA BARRIGA</span>
-    <span><a target="_blank" href="https://github.com/gregoriorondon/readygrade-desktop">Descargar Versión De Escritorio<i class="fa-brands fa-github"></i></a></span>
+</div>
+<div class="desk">
+    <span><a target="_blank" href="https://github.com/gregoriorondon/readygrade-desktop">Descargar Versión De Escritorio <i class="fa-brands fa-github"></i></a></span>
 </div>
