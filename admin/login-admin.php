1ed54725c1c8fa4c20c5f9ecc4fb97b9c6169d43
gregoriorondon <gregorioalexanderondon@gmail.com>
2024-01-23 16:11:21 -0400

    admin update
    se agrego una advertencia en la consola de JavaScript a las secciones de los estudiantes y el administrador ademas se agrego un diseno responsivo al admin y a su login

--- admin/login-admin.php
+++ admin/login-admin.php
@@ -6,6 +6,12 @@
     <title>Login Administrador</title>
     <link rel="stylesheet" href="./css-admin/estilos-login.css">
     <?php include './elements-admin/import.php'?>
+    <script>
+        console.warn("Cuidado Usuario");
+        console.warn("Si eres un usuario normal POR FAVOR NO USES ESTA CONSOLA")
+        console.log("%c%s","color:red;background-color:yellow;font-size:25px;border-radius:10px;padding:0px 7px 0px 7px;","Cuidado!!!");
+        console.log("%c%s","font-size: 18px;","Si utilizas esta consola, otras personas podr\u00edan hacerse pasar por ti y robarte datos mediante un ataque Self-XSS.\nNo escribas ni pegues ning\u00fan c\u00f3digo que no entiendas.");
+    </script>
 </head>
 <body>
 <?php include './elements/nav.php'?>
--- estudiantes.php
+++ estudiantes.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estudiantes</title>
     <?php include './elements/import.php'?>
+    <script>
+        console.warn("Cuidado Usuario");
+        console.warn("Si eres un usuario normal POR FAVOR NO USES ESTA CONSOLA")
+        console.log("%c%s","color:red;background-color:yellow;font-size:25px;border-radius:10px;padding:0px 7px 0px 7px;","Cuidado!!!");
+        console.log("%c%s","font-size: 18px;","No utilices esta consola, no escribas ni pegues ning\u00fan c\u00f3digo o script.");
+    </script>
 </head>
 <body class="cuerpo">
 <?php include './elements/nav.php'?>
