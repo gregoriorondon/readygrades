f8ae6c3db947cb7df75d981d3784718f44f9a62c
gregoriorondon <gregorioalexanderondon@gmail.com>
2024-06-06 14:51:52 -0400

    nuevos cambios

--- elements/nav.php
+++ elements/nav.php
@@ -1,8 +1,7 @@
 <!--botton con la opcion de regresar a pa pagina anteriormente visitada-->
 
 <nav class="menu">
-<button onclick="history.back();" class="back"><i class="fa-solid fa-arrow-left-long"></i>Regresar</button>
-<button onclick="history.forward();" class="forward">Volver<i class="fa-solid fa-arrow-right"></i></button>
+<button class="home"><img src="../img/logouptt.png" alt="" style="width: 37px; display: inline-block; vertical-align: middle;">Inicio</button>
 <button class="m-student"><i class="fa-solid fa-graduation-cap"></i>Estudiante</button>
 <button class="m-teacher"><i class="fa-solid fa-chalkboard-user"></i>Profesor</button>
 
