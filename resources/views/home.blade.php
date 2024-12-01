2672cae54df44f41dec963c30cca5ac92b56c6a3
gregoriorondon <gregorioalexanderondon@gmail.com>
2024-12-01 18:47:22 -0400

    modificaciones en el inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,21 +16,29 @@
         <h2>"MARIO BRICEÑO IRAGORRY"</h2>
     </center>
 </div>
-<section class="sec-about">
-    <div class="i-about">
-        <img class="img-about" title="Education illustrations by Storyset (https://storyset.com/education)" src="./Classroom-rafiki.png" alt="https://storyset.com/education">
-    </div>
-    <div class="s-about">
-        <h1>Misión</h1>
-        <p>En la UPTT, se debe formar a los jóvenes para continuar sus estudios a nivel superior, preparándolos para la vida adulta tomando en cuenta la diversidad de talentos y el estudio para lograr una educación de buena calidad que responda a los lineamientos educativos racionales.</p>
-        <h1>Visión</h1>
-        <p>La universidad Politécnica Territorial de Estado Trujillo “Mario Briceño Iragorry”, es una institución enmarcada dentro de la Educación Media General y de formación profesional que tiene como alcance una educación de calidad que dé respuesta a la formación del individuo, profundizando en los conocimientos científicos y humanísticos.</p>
+<section class="sec-readygrades">
+    <div class="readygrades">
+        <h1>Ver Tus Notas Académicas</h1>
+        <p>El Área de Registro, Seguimiento y Control de Estudios, en conjunto con el sistema ReadyGrades, desarrollado por estudiantes de Ingeniería en Informática en colaboración con profesionales del área administrativa, ha implementado una plataforma innovadora para la visualización de calificaciones académicas, destinada a los estudiantes matriculados en nuestra institución.</p>
+        <a href="/estudent"><button class="readylink"><i class="fa-solid fa-graduation-cap"></i>Estudiante</button></a>
     </div>
 </section>
-
-<section class="historia">
-    <div class="s-hist">
-        <h1>Historia  de la Institución</h1>
+<section class="sec-about">
+    <!-- <div class="i-about"> 
+         <img class="img-about" title="Education illustrations by Storyset (https://storyset.com/education)" src="./Classroom-rafiki.png" alt="https://storyset.com/education"> 
+     </div> -->
+    <div class="s-about">
+    <h1>Sobre Nosotros</h1>
+    <details open>
+        <summary>Misión</summary>
+        <p>En la UPTT, se debe formar a los jóvenes para continuar sus estudios a nivel superior, preparándolos para la vida adulta tomando en cuenta la diversidad de talentos y el estudio para lograr una educación de buena calidad que responda a los lineamientos educativos racionales.</p>
+    </details>
+    <details>
+        <summary>Visión</summary>
+        <p>La universidad Politécnica Territorial de Estado Trujillo “Mario Briceño Iragorry”, es una institución enmarcada dentro de la Educación Media General y de formación profesional que tiene como alcance una educación de calidad que dé respuesta a la formación del individuo, profundizando en los conocimientos científicos y humanísticos.</p>
+    </details>
+    <details>
+        <summary>Historia  de la Institución</summary>
         <p>Debido al gran déficit de cupos Universitarios el Instituto Universitario de Tecnología del Estado Trujillo "Don Rómulo Betancourt" se generó un plan de Expansión a Nivel del Estado, para ofrecer otra alternativa de estudios superiores y contribuir al desarrollo potencial del Estado. Gracias a las gestiones de Carlos Simón Olmos se logró establecer en el Dividive la Sede según la Resolución del Ministerio de Educación Publicada en gaceta oficial el 16 de Noviembre de 1988.
             <br class="salto">
             La Extensión del IUTET Zona Baja, inicia sus actividades académicas formando Técnicos Superiores Universitarios en la Carrera de Administración de Empresas, con un Programa de Inducción para los alumnos del curso Propedéutico A-89, el 3 de Abril de 1989, en la sede del Liceo “Doña Fulgencia Jiménez de Olmos”, teniendo a su disposición dos aulas de clase y dos oficinas, dicho curso tuvo una duración de 14 semanas con una matrícula de 120 alumnos dividido en dos turnos con un horario de 1:00pm a 6:00pm y de 6:00pm a 11:00pm para entonces la Extensión El Dividive contaba con un (1) Coordinador Prof Vistrimo Hidalgo y siete (7) Docentes, Nohelia Ascanio, Pbro Pedro Balza, Ines Barrios, María Pacheco, Gioconda Miele, Juan Ramírez y Alexandra Theodorou.
@@ -42,7 +50,13 @@
             En el mes de Octubre del 2008 el IUTET se integra al proceso de transformación de los Institutos y Colegios Universitarios en Universidades, con la coordinación de la Lcda. Mary Marin en el PNF de Administración y el Ingeniero José Fernández en el PNF de Mantenimiento, lo cual implica mucho más que el establecimiento de las llamadas carreras largas. En el centro del cambio está la intención de crear universidades comprometidas con el país y sus ciudadanos, capaces de generar, transformar y apropiar conocimiento socialmente útil y adecuado, que sirva de sustento al proceso de cambio que vive la nación, reconstruyendo su base ética, social, cultural y productiva para el logro de una sociedad justa e igualitaria.
             <br class="salto">
             El 2 de mayo del 2014 decreto N.936 el Presidente Nicolás Maduro pública en Gaceta Oficial, que los IUTET pasan a ser Universidades Politécnicas, quedando nuestra extensión como Universidad Politécnica Territorial del Estado Trujillo (U.P.T.T) Mario Briceño Iragorry núcleo Francisco de Miranda, en el Marco de la Misión Ama Mater, como Universidad Nacional Experimental, mediante la articulación de la Misión Sucre e Instituciones de Educación Universitaria.</p>
+    </details>
     </div>
+</section>
+<section class="map">
+    <h1>Nuestra Ubicación</h1>
+    <p>Nuestra institución se encuentra ubicada en El Dividive, Municipio Miranda, Estado Trujillo.</p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1967.6988538974847!2d-70.74086259549723!3d9.474093480588985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e62dc5d4b15c6a9%3A0xa44815ca92d62255!2sInstituto%20Universitario%20de%20Tecnolog%C3%ADa%20del%20Estado%20Trujillo!5e0!3m2!1ses!2sve!4v1703086452751!5m2!1ses!2sve" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map-1"></iframe>
 </section>
 <x-footer>
 </x-footer>
