a6c52603afae69d902f74940d44a25655ae17ecd
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-05-04 13:56:31 -0400

    Edicion fromulario estudiante
    Se edito el formulario del registro de estudiantes para resaltar los campos obligatorios de los opcionales al inscribir el estudiante.

--- resources/views/components/form-student.blade.php
+++ resources/views/components/form-student.blade.php
@@ -1,14 +1,5 @@
 <form method="POST" action="/registro-estudiante">
     @csrf
-    {{-- @if ($errors->any()) --}}
-    {{-- <div class="alert alert-danger"> --}}
-    {{--     <ul> --}}
-    {{--         @foreach ($errors->all() as $error) --}}
-    {{--             <li>{{ $error }}</li> --}}
-    {{--         @endforeach --}}
-    {{--     </ul> --}}
-    {{-- </div> --}}
-    {{-- @endif --}}
   <div class="space-y-12 p-[21px]">
     <div class="border-gray-900/10 pb-12">
       <p class="mt-7 text-xl font-inter text-gray-400">Rellene todas las casillas para registrar al nuevo estudiante en la institución</p>
@@ -18,21 +9,21 @@
         <div class="sm:col-span-3">
           <div class="mt-2">
           <x-label>Nombre Completo</x-label>
-            <x-input-form type="text" name="primer_name" id="first-name" placeholder="Primer Nombre" :value="old('primer_name')" required />
-            <x-input-form type="text" name="segundo_name" id="first-name" placeholder="Segundo Nombre" :value="old('segundo_name')" />
+            <x-input-form type="text" name="primer_name" id="first-name" placeholder="Primer Nombre (Obligatorio)" :value="old('primer_name')" required autocomplete="off" />
+            <x-input-form type="text" name="segundo_name" id="first-name" placeholder="Segundo Nombre" :value="old('segundo_name')" autocomplete="off" />
           </div>
         </div>
 
         <div class="sm:col-span-3">
           <div class="mt-2">
           <x-label>Apellido Completo</x-label>
-            <x-input-form type="text" name="primer_apellido" id="last-name" placeholder="Primer Apellido" :value="old('primer_apellido')" required  />
-            <x-input-form type="text" name="segundo_apellido" id="last-name" placeholder="Segundo Apellido" :value="old('segundo_apellido')" />
+            <x-input-form type="text" name="primer_apellido" id="last-name" placeholder="Primer Apellido (Obligatorio)" :value="old('primer_apellido')" required autocomplete="off"  />
+            <x-input-form type="text" name="segundo_apellido" id="last-name" placeholder="Segundo Apellido" :value="old('segundo_apellido')" autocomplete="off" />
           </div>
         </div>
 
         <div class="sm:col-span-1 mt-2 sm:col-start-1">
-        <x-label>Genero / Sexo</x-label>
+        <x-label class="after:content-['*'] after:text-red-400">Genero / Sexo</x-label>
             <div class="flex items-center">
             <x-select-form class="sm:max-w-full" name="genero" :value="old('genero')">
                 <option value="masculino">Masculino</option>
@@ -41,10 +32,9 @@
             </div>
         </div>
 
-
         <div class="sm:col-span-1">
           <div class="mt-2">
-          <x-label>Nacionalidad</x-label>
+          <x-label class="after:content-['*'] after:text-red-400">Nacionalidad</x-label>
             <x-select-form  class="sm:max-w-full" id="nacionalidad" name="nacionalidad" >
                 <option value="VE">Venezolano(a)</option>
                 <option value="EX">Extranjero(a)</option>
@@ -54,15 +44,15 @@
 
         <div class="sm:col-span-2">
           <div class="mt-2">
-          <x-label>Cédula de Identidad</x-label>
-            <x-input-form class="sm:max-w-full" type="number" name="cedula" id="cedula" placeholder="Número de Cedula del Estudiante" :value="old('cedula')" required  />
+          <x-label class="after:content-['*'] after:text-red-400">Cédula de Identidad</x-label>
+            <x-input-form class="sm:max-w-full" type="number" name="cedula" id="cedula" placeholder="Número de Cedula del Estudiante" :value="old('cedula')" required autocomplete="off"  />
             <x-input-error name="cedula" />
           </div>
         </div>
 
         <div class="sm:col-span-2">
           <div class="mt-2">
-          <x-label>Fecha de Nacimiento</x-label>
+          <x-label class="after:content-['*'] after:text-red-400">Fecha de Nacimiento</x-label>
             <x-input-form class="sm:max-w-full" type="date" name="fecha_nacimiento" id="fecha-nacimiento" :value="old('fecha_nacimiento')" required  />
           </div>
         </div>
@@ -70,28 +60,28 @@
         <div class="sm:col-span-2 sm:col-start-1">
           <div class="mt-2">
           <x-label>Correo / Email</x-label>
-            <x-input-form type="email" name="email" id="email" placeholder="Dirección de Correo Electrónico del Estudiante" :value="old('email')"  />
+            <x-input-form type="email" name="email" id="email" placeholder="Dirección de Correo Electrónico del Estudiante" :value="old('email')"  autocomplete="off" />
           </div>
         </div>
 
         <div class="sm:col-span-1">
           <div class="mt-2">
           <x-label>Teléfono</x-label>
-            <x-input-form type="tel" name="telefono" id="telefono" placeholder="Teléfono del Estudiante" :value="old('telefono')"  />
+            <x-input-form type="tel" name="telefono" id="telefono" placeholder="Teléfono del Estudiante" :value="old('telefono')"  autocomplete="off" />
           </div>
         </div>
 
         <div class="sm:col-span-2">
           <div class="mt-2">
-          <x-label>Dirección</x-label>
-            <x-input-form type="text" name="direccion" id="direccion" placeholder="Dirección del Estudiante" :value="old('direccion')" required  />
+          <x-label class="after:content-['*'] after:text-red-400">Dirección</x-label>
+            <x-input-form type="text" name="direccion" id="direccion" placeholder="Dirección del Estudiante" :value="old('direccion')" required  autocomplete="off" />
           </div>
         </div>
 
         <div class="sm:col-span-1">
           <div class="mt-2">
-          <x-label>Ciudad</x-label>
-            <x-input-form type="text" name="city" id="city" placeholder="Ciudad o Pueblo del Estudiante" :value="old('city')" required  />
+          <x-label class="after:content-['*'] after:text-red-400">Ciudad</x-label>
+            <x-input-form type="text" name="city" id="city" placeholder="Ciudad o Pueblo del Estudiante" :value="old('city')" required  autocomplete="off" />
           </div>
         </div>
 
@@ -99,7 +89,7 @@
         <div class="carreratramonucleo">
           <div class="mt-2">
 
-          <x-label>Carrera a Estudiar</x-label>
+          <x-label class="after:content-['*'] after:text-red-400">Carrera a Estudiar</x-label>
           @props(['courses', 'trayectos', 'nucleos'])
           <x-select-form class="sm:max-w-full" name="carrera_id" id="carreras_id" >
                 @foreach($courses as $carrera)
@@ -110,7 +100,7 @@
         </div>
         <div class="carreratramonucleo">
           <div class="mt-2">
-          <x-label>Tramo y Trayecto</x-label>
+          <x-label class="after:content-['*'] after:text-red-400">Tramo y Trayecto</x-label>
             <x-select-form class="sm:max-w-full" name="tramo_id">
                 @foreach($trayectos as $trayecto)
                     <optgroup label="{{ $trayecto->trayectos }}">
@@ -124,7 +114,7 @@
         </div>
         <div class="carreratramonucleo">
           <div class="mt-2">
-          <x-label>Núcleos</x-label>
+          <x-label class="after:content-['*'] after:text-red-400">Núcleos</x-label>
             <x-select-form class="sm:max-w-full" name="nucleo_id">
                 @foreach($nucleos as $nucleo)
                     <option value="{{ $nucleo->id }}">{{ $nucleo->nucleo }}</option>
