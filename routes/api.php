e0a89d47f9d45bce43f12f9c2e2e5ebde0129df8
Gregorio Rondon <gregorioalexanderondon@gmail.com>
2025-05-28 12:38:34 -0400

    cambio de rutas de web.php a api.php
    Se cambió el inicio y el organigrama de la vista principal del proyecto
    para que no guarde ningun "token" en la base de datos o cookie para
    evitar estar almacenando información innecesaria de estas vistas ya que
    no hay procedminientos de seguridad como en los formularios o consultas
    en las base de datos

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\UniversityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(UniversityController::class)->group( function (){
+    Route::get('/', 'index');
+    Route::get('/organigrama', 'organigrama');
+    /* Route::get('/login-admin', 'admin'); */
+    /* Route::get('/student', 'students'); */
+    /* Route::post('/detalles-estudiante', 'studentspublicdetails'); */
+    /* Route::get('/registro-profesor', 'teacheradd'); */
+});
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 /* }); */
 
 Route::controller(UniversityController::class)->group( function (){
-    Route::get('/', 'index');
-    Route::get('/organigrama', 'organigrama');
+    /* Route::get('/', 'index'); */
+    /* Route::get('/organigrama', 'organigrama'); */
     Route::get('/login-admin', 'admin');
     Route::get('/student', 'students');
     Route::post('/detalles-estudiante', 'studentspublicdetails');
