62b53d04e01dd02db5b4e6e52e8aa2532dc54b4f
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-06-22 14:29:53 -0400

    Change: inicio de sesion
    Se cambió en el menu o barra de navegación el boton de "profesores" a "Iniciar Sesión" con su respectivo icono de iniciar sesión, ademas de que se cambió la ruta de este, para que administradores y profesores puedan iniciar sesión en el mismo login.

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -4,7 +4,7 @@
         <a href="/" class="{{ request()->is('/') ? 'underline underline-offset-8 decoration-4' : 'no-underline' }}"><i class="fa-solid fa-house"></i>Inicio</a>
         <a href="/organigrama" class="{{ request()->is('organigrama') ? 'underline underline-offset-8 decoration-4' : 'no-underline' }}"><i class="fa-solid fa-sitemap"></i>Organigrama</a>
         <a href="/student" class="{{ request()->is('student') ? 'underline underline-offset-8 decoration-4' : 'no-underline' }}"><i class="fa-solid fa-user-graduate"></i>Estudiante</a>
-        <a href="/login-profesor" class="{{ request()->is('login-profesor') ? 'underline underline-offset-8 decoration-4' : 'no-underline' }}"><i class="fa-solid fa-person-chalkboard"></i>Profesor</a>
+        <a href="/login" class="{{ request()->is('login-profesor') ? 'underline underline-offset-8 decoration-4' : 'no-underline' }}"><i class="fa-solid fa-right-to-bracket"></i>Iniciar Sesión</a>
     </nav>
 </div>
 
@@ -21,7 +21,7 @@
         <a href="/"><i class="fa-solid fa-house"></i>Inicio</a>
         <a href="/organigrama"><i class="fa-solid fa-sitemap"></i>Organigrama</a>
         <a href="/student"><i class="fa-solid fa-user-graduate"></i>Estudiante</a>
-        <a href="/login-profesor"><i class="fa-solid fa-person-chalkboard"></i>Profesor</a>
+        <a href="/login"><i class="fa-solid fa-right-to-bracket"></i>Iniciar Sesión</a>
     </nav>
 </div>
 <script src="{{ Vite::asset('resources/js/closemenuhome.js') }}"></script>
