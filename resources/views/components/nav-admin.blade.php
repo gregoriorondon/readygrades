c19f91181fd1ee2d6a456be3d31072cf29dffc8e
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-06-23 16:39:56 -0400

    Fixed or Feature: menu de navegacion admin y profesor
    Se simplificó un poco mas el código de los componentes haciendo que solo
    se pase una sola vez la dirección de enlace y el icono del menu se
    colocó para solo pasar el nombre mas no la etiqueta completa que genera
    FontAwesome.

--- resources/views/components/nav-admin.blade.php
+++ resources/views/components/nav-admin.blade.php
@@ -1,19 +1,24 @@
-<x-sidebar-section href="/administracion" :active="request()->is('administracion')"><i class="fa-solid fa-house"></i>Dashboard</x-sidebar-section>
-<details class="register-add-sidebar-details">
-    <summary class="selection:bg-transparent cursor-pointer px-4 py-2 mt-2 font-inter"><i class="fa-solid fa-address-book"></i>Registros</summary>
-    <x-sidebar-section href="/registro-estudiante" :active="request()->is('registro-estudiante')"><i class="fa-solid fa-square-plus"></i>Registrar Estudiantes</x-sidebar-section>
-    <x-sidebar-section href="/registro-profesor" :active="request()->is('registro-profesor')"><i class="fa-solid fa-user-plus"></i>Registrar Profesor</x-sidebar-section>
-    <x-sidebar-section href="/registro-administrador" :active="request()->is('registro-administrador')"><i class="fa-solid fa-address-card"></i>Registrar Administrador</x-sidebar-section>
+@cannot('profesor')
+    <x-sidebar-section link="administracion" icon="fa-solid fa-house">Dashboard</x-sidebar-section>
+    <details class="register-add-sidebar-details">
+        <summary class="selection:bg-transparent cursor-pointer px-4 py-2 mt-2 font-inter"><i class="fa-solid fa-address-book"></i>Registros</summary>
+        <x-sidebar-section link="registro-estudiante" icon="fa-solid fa-square-plus">Registrar Estudiantes</x-sidebar-section>
+        <x-sidebar-section link="registro-profesor" icon="fa-solid fa-user-plus">Registrar Profesor</x-sidebar-section>
+        <x-sidebar-section link="registro-administrador" icon="fa-solid fa-address-card">Registrar Administrador</x-sidebar-section>
+        @can('root')
+            <x-sidebar-section link="agregar-cargo" icon="fa-solid fa-briefcase">Agregar Un Cargo</x-sidebar-section>
+        @endcan
+    </details>
+    <x-sidebar-section link="estudiantes-panel-administrativo" icon="fa-duotone fa-solid fa-user-graduate">Estudiantes</x-sidebar-section>
+    <x-sidebar-section link="nomina-profesores" icon="fa-duotone fa-regular fa-chalkboard-user">Profesores</x-sidebar-section>
+    <x-sidebar-section link="nomina-administradores" icon="fa-solid fa-user-tie">Administradores</x-sidebar-section>
     @can('root')
-        <x-sidebar-section href="/agregar-cargo" :active="request()->is('agregar-cargo')"><i class="fa-solid fa-briefcase"></i>Agregar Un Cargo</x-sidebar-section>
+        <x-sidebar-section link="carreras" icon="fa-solid fa-graduation-cap">Carreras</x-sidebar-section>
+        <x-sidebar-section link="nucleos" icon="fa-solid fa-signs-post">Núcleos</x-sidebar-section>
+        <x-sidebar-section link="tramos-y-trayectos" icon="fa-solid fa-list-timeline">Trayectos y Tramos</x-sidebar-section>
     @endcan
-</details>
-<x-sidebar-section href="/estudiantes-panel-administrativo" :active="request()->is('estudiantes-panel-administrativo')"><i class="fa-duotone fa-solid fa-user-graduate"></i>Estudiantes</x-sidebar-section>
-<x-sidebar-section href="/nomina-profesores" :active="request()->is('nomina-profesores')"><i class="fa-duotone fa-regular fa-chalkboard-user"></i>Profesores</x-sidebar-section>
-<x-sidebar-section href="/nomina-administradores" :active="request()->is('nomina-administradores')"><i class="fa-solid fa-user-tie"></i>Administradores</x-sidebar-section>
-@can('root')
-    <x-sidebar-section href="/carreras" :active="request()->is('carreras')"><i class="fa-solid fa-graduation-cap"></i>Carreras</x-sidebar-section>
-    <x-sidebar-section href="/nucleos" :active="request()->is('nucleos')"><i class="fa-solid fa-signs-post"></i>Núcleos</x-sidebar-section>
-    <x-sidebar-section href="/tramos-y-trayectos" :active="request()->is('tramos-y-trayectos')"><i class="fa-solid fa-list-timeline"></i>Trayectos y Tramos</x-sidebar-section>
+    <x-sidebar-section link="generar-documentos" icon="fas fa-file-plus">Generar</x-sidebar-section>
+@endcannot
+@can('profesor')
+    <x-sidebar-section link="dashboard" icon="fa-solid fa-house">Dashboard</x-sidebar-section>
 @endcan
-<x-sidebar-section href="/generar-documentos" :active="request()->is('generar-documentos')"><i class="fas fa-file-plus"></i>Generar</x-sidebar-section>
--- resources/views/components/sidebar-section.blade.php
+++ resources/views/components/sidebar-section.blade.php
@@ -1,3 +1,10 @@
-@props(['active' => false])
+@props(['active'=>false, 'link'=>'', 'icon' => ''])
 
-<a class="{{ $active ? 'bg-gray-700 hover:bg-gray-500': 'hover:bg-gray-700 hover:rounded-xl' }} transition-all flex items-center rounded-xl px-4 py-2 mt-2 font-inter" aria-current="{{ $active ? 'page': 'false'}}"{{ $attributes }}>{{ $slot }}</a>
+@php
+    $active = request()->is($link) || request()->routeIs($link);
+@endphp
+
+<a href="/{{ $link }}" class="{{ $active ? 'bg-gray-700 hover:bg-gray-500' : 'hover:bg-gray-700 hover:rounded-xl' }} transition-all flex items-center rounded-xl px-4 py-2 mt-2 font-inter"
+    aria-current="{{ $active ? 'page' : 'false' }}"{{ $attributes }}><i class="{{ $icon }}"></i>
+    {{ $slot }}
+</a>
