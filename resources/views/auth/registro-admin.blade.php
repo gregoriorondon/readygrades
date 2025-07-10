1c68eab56300708272ab9e4e6d8869a69e464589
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-07-10 19:01:00 -0400

    Auth: cargos
    Se asignó un parámetro para que el único administrador que tenga acceso
    a crear un cargo desde la creación de un profesor o administrador

--- resources/views/auth/registro-admin.blade.php
+++ resources/views/auth/registro-admin.blade.php
@@ -1,7 +1,10 @@
 <x-dashboard>
     <x-slot:titulo>Registrar Nuevo Administrador</x-slot:titulo>
-    <x-button-a class="btn-new-student" link="agregar-cargo" icon="fa-solid fa-plus-large">Agregar Un Nuevo
-        Cargo</x-button-a>
+    @can('root')
+        <x-button-a class="btn-new-student" link="agregar-cargo" icon="fa-solid fa-plus-large">
+            Agregar Un Nuevo Cargo
+        </x-button-a>
+    @endcan
     <x-title-section-admin>Registro de Nuevo Administrador</x-title-section-admin>
     <x-form-admin-register :cargo="$cargo" :estudio="$estudio" :nucleo="$nucleo" />
     <x-error-and-correct-dialog />
--- resources/views/auth/registro-profesor.blade.php
+++ resources/views/auth/registro-profesor.blade.php
@@ -1,6 +1,8 @@
 <x-dashboard>
     <x-slot:titulo>Registrar Nuevo Profesor</x-slot:titulo>
-    <x-button-a class="btn-new-student" link="agregar-cargo" icon="fa-solid fa-plus-large">Agregar Un Nuevo Cargo</x-button-a>
+    @can('root')
+        <x-button-a class="btn-new-student" link="agregar-cargo" icon="fa-solid fa-plus-large">Agregar Un Nuevo Cargo</x-button-a>
+    @endcan
     <x-title-section-admin>Registro de Nuevo Profesor</x-title-section-admin>
     <x-form-teacher-register :cargo="$cargo" :estudio="$estudio" :nucleo="$nucleo" />
     <x-error-and-correct-dialog />
