460c6837a2e0773f083754aeb86dc8db1b6cc7db
gregoriorondon <gregorio@pc.dell.optiplex>
2025-09-07 12:19:35 -0400

    Feature: Select None Text Student Section

--- resources/views/auth/students.blade.php
+++ resources/views/auth/students.blade.php
@@ -20,12 +20,12 @@
     </x-button>
 </form>
 
-<x-title-section-admin>Listado De Todos Los Estudiantes</x-title-section-admin>
+<x-title-section-admin class="select-none">Listado De Todos Los Estudiantes</x-title-section-admin>
     <div class="">
         <div class="mt-2 border border-gray-200 border-solid rounded-lg">
             <div class="">
                 <div class="flex flex-col overflow-x-auto">
-                    <table class="">
+                    <table class="select-none">
                         <thead class="border-b">
                             <tr>
                                 <x-table-th-students>
