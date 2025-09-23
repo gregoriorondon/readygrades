39606fefe4f419cbd9c5e67879a3b9ef9c57040f
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-09-23 12:38:41 -0400

    add: menu component

---
+++ resources/views/components/menuuptt.blade.php
@@ -0,0 +1,48 @@
+ <div class="menu">
+     <div class="menus justify-between items-center">
+         <div class="w-[15%]">
+             <a href="/">
+                 <img src="/cintilloUPTT2.webp">
+             </a>
+         </div>
+         <div class="text-center">
+             <nav class="menu-wrapper">
+                 <ul class="main-menu" id="mobile-menu">
+                     <li class="!ml-0"><a href="/">INICIO</a></li>
+                     <li><a href="autoridades.html">AUTORIDADES</a></li>
+                     <li><a href="nucleos/index.html">NÚCLEOS</a></li>
+                     <li><a href="pnf/index.html">PNF</a></li>
+                     <li><a href="drsce/drsce.html">DRSCE</a></li>
+                     <li><a href="#contact-us">CONTACTO</a></li>
+                     <li><a href="/login">LOGIN</a></li>
+                     {{-- <li> <img src="/200.png" class="h-[40px]"></i> --}}
+                     </li>
+                 </ul>
+             </nav>
+         </div>
+         <div class="w-[5%]">
+             <a href="/">
+                 <img src="/200.png">
+             </a>
+         </div>
+     </div>
+ </div>
+ <script>
+     // Espera a que el DOM (Document Object Model) esté cargado
+     document.addEventListener('DOMContentLoaded', function() {
+         // Selecciona el elemento del menú
+         const menu = document.querySelector('.menu');
+
+         // Agrega un evento 'scroll' a la ventana
+         window.addEventListener('scroll', function() {
+             // Si el desplazamiento vertical (scrollY) es mayor que 50px...
+             if (window.scrollY > 50) {
+                 // ...añade la clase 'menu-encogido'
+                 menu.classList.add('menu-encogido');
+             } else {
+                 // ...si no, quita la clase 'menu-encogido'
+                 menu.classList.remove('menu-encogido');
+             }
+         });
+     });
+ </script>
