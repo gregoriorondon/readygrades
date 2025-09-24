b11ac6c65feba8248a706c87bdfeb8b5cc48cb51
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-09-23 21:09:13 -0400

    modificaciones en el foot
    Se agrego un nuevo componente para tener el foot original pero admeas de
    esto se le hicieron algunas modificaciones

---
+++ resources/views/components/footer-original.blade.php
@@ -0,0 +1,108 @@
+    <footer class="footer-area pt-60 pb-60 black-bg" id="contact-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget">
+                        <div class="footer-logo">
+                            <a href="index.html">UPTTMBI </a>
+                        </div>
+                        <div class="footer-dec">
+                            <h3>
+                                <p><strong style= "color: white">Excelencia Académica para Contruir
+                                        Patria.</strong><br><br>Sigue Nuestras Redes Sociales:</p>
+                            </h3>
+                        </div>
+                        <ul class="social-links">
+                            <li><a href="https://www.facebook.com/upttmbi/" target="_blank">
+                                    <i class="fab fa-facebook-f"></i>Gestión UPTTMBI</a></li><br>
+                            <li><a href="https://www.twitter.com/upttmbi" target="_blank">
+                                    <i class="fab fa-twitter"></i>@upttmbi</a></li><br>
+                            <li><a href="https://www.instagram.com/upttmbi" target="_blank">
+                                    <i class="fab fa-instagram"></i>@upttmbi</a></li><br>
+                            <li><a href="https://www.youtube.com/channel/UC7Afc16iAxmn_XcKgV5EFWQ" target="_blank">
+                                    <i class="fab fa-youtube"></i>@upttmbi</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-9 col-md-15">
+                    <div class="footer-widget">
+                        <div class="widget-title">
+                            <h6>Ubicanos:</h6>
+                        </div>
+                        <div class="address-line">
+                            <p>Dirección: Núcleo San Luis (Sede Rectoral)<br>Av. La Feria Sector San Luis Valera, Estado
+                                Trujillo - Venezuela</p>
+                            <p><B>Teléfonos:</B> Rectorado (58) 0271-221.29.27, Vicerrectorado Académico 0271-221.51.47,
+                                Secretaría 0271-221.29.67<br>
+                                <b>Núcleos:</b><br> Boconó: Calle Colón con Av. Independencia (Antigo Hospital "Rafael
+                                Rangel") Teléfono: 0272-652.31.11
+                                <br>El Dividive: Autopista Panamericana, El Dividive. Teléfono: 0272-666.01.14
+                                <br>La Beatriz: Urb. La Beatriz Parte Alta, al lado del Parque Botánico. Telefono:
+                                0271-231.11.10
+                                <br>Trujillo: Av. Cristóbal Mendoza detrás del Mercado Municipal. Teléfono:
+                                0272-236.31.61
+                                <br>Carache: Calle Vargas entre Av.2 y Av.3, al lado Alcaldía del Municipio Carache.
+                                Teléfono: 0272-999.16.05
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer Area -->
+    <!-- End Copyright Area -->
+    <div class="copyright-area black-bg">
+        <div class="container">
+            <div class="row ">
+                <div class="col-12 text-center">
+                    <div class="copyright-area ">
+                        <p>{{ ucwords('UPTTMBI © 2025. RIF G-20005902-8 NIT 04865655101.') }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Copyright Area -->
+    <!-- JS -->
+    <!-- Popper.js -->
+    <script src="assets/js/popper.min.js"></script>
+    <!-- Bootstrap.js -->
+    <script src="assets/js/bootstrap-4.3.1.min.js"></script>
+    <!-- Modernizr.js -->
+    <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Owl.Carousel.js -->
+    <script src="assets/js/vendor/owl.carousel-2.3.4.min.js"></script>
+    <script src="assets/js/vendor/owl.carousel2.thumbs.min.js"></script>
+    <!-- Waypoints.js -->
+    <script src="assets/js/vendor/waypoints-4.0.1.min.js"></script>
+    <!-- ColorNip.js -->
+    <script src="assets/js/vendor/colornip.min.js"></script>
+    <!-- SlickNav.js -->
+    <script src="assets/js/vendor/slicknav.min.js"></script>
+    <!-- ScrollUp.js -->
+    <script src="assets/js/vendor/jquery.scrollUp.min.js"></script>
+    <!-- MagnificPopup.js -->
+    <script src="assets/js/vendor/magnific-popup.min.js"></script>
+
+    <!-- Main.js -->
+    <script src="assets/js/main.js"></script>
+
+    <script src="../../cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+
+    {{-- <script> --}}
+    {{--     document.addEventListener('contextmenu', event => event.preventDefault()); --}}
+    {{--     document.addEventListener('keydown', function(event) { --}}
+    {{--         if ( --}}
+    {{--             event.key === 'F12' || --}}
+    {{--             (event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'J')) || --}}
+    {{--             (event.ctrlKey && event.key === 'U') --}}
+    {{--         ) { --}}
+    {{--             event.preventDefault(); --}}
+    {{--         } --}}
+    {{--     }); --}}
+    {{-- </script> --}}
