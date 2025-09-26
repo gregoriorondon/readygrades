4ad2293907b0abbe2510b554b69f9a083c1ea80c
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-09-26 18:44:48 -0400

    fixed: correcciones minimas en estudiante

--- resources/views/estudiantes.blade.php
+++ resources/views/estudiantes.blade.php
@@ -1,25 +1,29 @@
     <title>Estudiantes</title>
     <x-import />
     <link rel="stylesheet" href="/css/menu.css">
+    @vite(['public/css/style.css'])
     <script>
         console.warn("Cuidado Usuario");
         console.warn("Si eres un usuario normal POR FAVOR NO USES ESTA CONSOLA")
         console.log("%c%s","color:red;background-color:yellow;font-size:25px;border-radius:10px;padding:0px 7px 0px 7px;","Cuidado!!!");
         console.log("%c%s","font-size: 18px;","No utilices esta consola, no escribas ni pegues ning\u00fan c\u00f3digo o script.");
     </script>
-<body class="cuerpo">
+<body class="">
     <x-menuuptt />
+    <div class="cuerpo">
     <div>
         <x-authentication-card>
             <x-slot name="logo">
                 <x-authentication-card-logo />
             </x-slot>
-            <h1 style="font-size: 40px; font-weight: 700; color: #4272D8;" class="font-inter uppercase">Ver sus Datos Académicos</h1>
+            <h1 style="font-size: 40px; font-weight: 700; color: #4272D8;"
+                class="font-inter uppercase !text-left leading-none">Ver sus Datos Académicos
+            </h1>
             <p class="font-inter mb-7">Introduzca una cédula válida, compuesta únicamente por números, sin incluir caracteres especiales.</p>
             <form method="POST" action="/detalles-estudiante">
                 @csrf
                 <div>
-                    <x-input type="number" name="cedula" placeholder="Ingrese su Cédula" autocomplete="off" />
+                    <x-input type="number" name="cedula" placeholder="Ingrese su Cédula" autocomplete="off" value="{{ old('cedula') }}" />
                     <x-input-error name="cedula" />
                 </div>
                 <div class="flex items-center justify-end mt-4">
@@ -33,6 +37,7 @@
             </div>
             </form>
         </x-authentication-card>
+    </div>
     </div>
 <x-minifoot />
 <x-footer-original />
