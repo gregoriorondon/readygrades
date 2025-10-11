e6b367b75589f8ff85d33420c6be9fa9120db653
gregoriorondon <gregorioalexanderondon@gmail.com>
2025-10-11 19:26:28 -0400

    Features: mejoras en el estilo del sitio

--- resources/views/autoridades.blade.php
+++ resources/views/autoridades.blade.php
@@ -6,6 +6,11 @@
     <title>Autoridades - UPTTMBI</title>
     <x-import />
     <style>
+        #speakers{
+            background-color: #f9f9f9;
+            padding-top: 3%;
+            padding-bottom: 12%;
+        }
         .owl-carousel {
             display: flex;
         }
@@ -19,6 +24,29 @@
             width: 400px;
             object-fit: cover;
         }
+        .speakers-thumb{
+            background-color: #fff;
+            padding-top: 14px;
+            padding-bottom: 14px;
+            text-align: center;
+        }
+        .speakers-thumb h3{
+            font-size: 150%;
+        }
+        .speakers-thumb h6{
+            font-size: 90%;
+        }
+        #overview{
+            background-color: #fff;
+        }
+        .row{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: left;
+            padding: 42px 0;
+            margin: 0 2.5rem;
+        }
     </style>
 </head>
 <body class="cuerpo" data-spy="scroll" data-offset="50" data-target=".navbar-collapse">
@@ -31,8 +59,8 @@
         <div class="container">
             <div class="">
 
-                <div class="mt-3 wow bounceIn">
-                    <div class="section-title text-center">
+                <div class="wow bounceIn">
+                    <div class="my-12 section-title text-center">
                         <h2>Autoridades Universitarias</h2>
                     </div>
                 </div>
@@ -85,6 +113,91 @@
             </div>
         </div>
     </section>
+
+    <!-- =========================
+    OVERVIEW SECTION
+============================== -->
+<section id="overview" class="parallax-section">
+    <center>
+	<div class="container">
+		<div class="row">
+			<div class="wow w-[60%] mr-12" data-wow-delay="0.6s">
+				<h2>Dra. Ninoska Ortiz</h2><h3>Rectora UPTTMBI</h3>
+				<p>Es la máxima autoridad ejecutiva de la Universidad y ejerce su representación legal.
+				<br><br><b>Atribuciones de la Rectora o el Rector:</b>
+				<br>1. Cumplir y hacer cumplir el ordenamiento jurídico vigente, las decisiones emanadas del Consejo Universitario y los lineamientos del Ministerio del Poder Popular con competencia en materia de educación universitaria.
+				<br>2. Representar a la Universidad en el Núcleo de Rectores del Consejo Nacional de Universidades.
+				<br>3. Velar por el cumplimiento del encargo social y los objetivos estratégicos de la Universidad, orientados hacia la transformacion...</p>
+			</div>
+			<div class="w-[40%]">
+				<img src="/img/rectora.webp" class="img-responsive">
+			</div>
+		</div>
+	</div>
+<hr>
+	<div class="container">
+		<div class="row">
+
+			<div class="wow mr-12 w-[40%]" data-wow-delay="0.6s">
+				<img src="/img/viceacad1.webp" class="img-responsive">
+			</div>
+
+			<div class="wow  w-[60%]" data-wow-delay="0.9s">
+				<h2>Dr. Alexis Peña</h2><h3>Vicerrector Académico</h3>
+				<p>Es la autoridad ejecutiva que asiste a la Rectora o al Rector en el gobierno y la coordinación de las actividades académicas de la Universidad.
+				<br><br><b>Atribuciones del Vicerrector Académico:</b>
+				<br><b>1.</b> Supervisar y coordinar con la Rectora o el Rector, las actividades de formación y de creación intelectual de conformidad con el Plan de Desarrollo Institucional de la Universidad.
+				<br><b>2.</b> Planificar, dirigir y promover estrategias de enseñanzas-aprendizaje para la formación permanente e integral de las y los estudiantes, docentes, Investigadores, personal administrativo y obrero.
+				<br><b>3.</b> Proponer al Consejo Universitario los cambios en la estructura académica en función de las necesidades de la misma.
+				<br><b>4.</b> Dirigir y supervisar el desarrollo y evaluación de los Programas de Formación en las dimensiones de desempeño estudiantil, currículo, estrategias pedagógicas, metodológicas  y didácticas, uso de tecnologías educativas, sistematización y socialización de experiencias  de acuerdo a las políticas y lineamientos emanados del Ministerio del Poder Popular con competencia en materia de educación universitaria.</p>
+				<p>Es el encargado de direccionar las Coordinaciones y/o direcciones de: <br>Formación Permanente y Docencia; Gestión Curricular de los Programas Nacionales de Formación y Formación Avanzada; Creación Intelectual y Desarrollo Socio Productivo; Recursos para la Formación, Núcleos y Extensiones, Gestión Académica de la Misión Sucre.</p>
+			</div>
+		</div>
+	</div>
+<hr>
+	<div class="container">
+		<div class="row">
+
+			<div class="wow w-[60%] mr-12" data-wow-delay="0.6s">
+				<h2>Dr. José González</h2><h3>Vicerrector de Desarrollo Territorial</h3>
+				<p>Es la autoridad ejecutiva que asiste a la Rectora o al Rector en el gobierno y la coordinación de las actividades vinculadas al desarrollo territorial de la Universidad.
+				<br><br><b>Atribuciones del Vicerrector de Desarrollo Territorial:</b>
+				<br><b>1.</b> Planificar, dirigir. Supervisar y evaluar las actividades para el desarrollo de proyectos de investigación vinculados con la realidad geohistórica, ambientales y culturales del área de influencia territorial
+				<br><b>2.</b> Evaluar los estudios situacionales del territorio, para generar criterios de desarrollo en la universidad. en el marco de la municipalización y Territorialización, en vinculación permanente con los objetivos del Plan de Desarrollo Económico y Social de la Nación.
+				<br><b>3.</b> Promover y articular en el territorio de influencia el desarrollo de las labores socio-académicas, de producción y creación de saberes e Integración socio-productiva que se realicen en la Universidad.
+				<br><b>4.</b> Instrumentar acciones político-académicas para Integrar a la Universidad a los planes y proyectos de desarrollo Impulsados por el Consejo Federal de Gobierno.</p>
+			</div>
+
+			<div class="wow w-[40%]" data-wow-delay="0.9s">
+				<img src="/img/viceterritorial.webp" class="img-responsive">
+			</div>
+		</div>
+	</div>
+<hr>
+
+	<div class="container">
+		<div class="row">
+
+			<div class="wow mr-12 w-[40%]">
+				<img src="/img/secretariageneral.webp">
+			</div>
+
+			<div class="wow w-[60%]">
+				<h2>MSc. Lisdreliz Godoy</h2><h3>Secretaria General</h3>
+				<p>Es la autoridad ejecutiva responsable de los asuntos concernientes a los sistemas de Información y documentación, así como de los procesos de registros y controles académicos de la Universidad.
+				<br><br><b>Atribuciones de la Secretaria General:</b>
+				<br><b>1.</b> Ejercer la secretaria de actas del Consejo Universitario.
+				<br><b>2.</b> Planificar y coordinar los procesos de ingreso, registro, prosecución y egreso de las y los estudiantes, a fin de garantizar la prestación eficaz y eficiente del servicio a los usuarios y cumplir con las exigencias legales.
+				<br><b>3.</b> Coordinar y supervisar el sistema de registro sobre la disponibilidad de la matricula estudiantil e Informar permanentemente al Ministerio del Poder Popular con competencia en materia de educación universitaria.
+				<br><b>4.</b> Planificar, coordinar y supervisar el buen funcionamiento de los Sistemas de Documentación de la Universidad.
+				<br><b>5.</b> Organizar y coordinar los actos de grados de la Universidad.
+				<br><b>6.</b> Cumplir con las Instrucciones impartidas por el Consejo Universitario y la Rectora o el Rector de la Universidad.</p>
+			</div>
+		</div>
+        </center>
+	</div>
+</section>
+
 
     <x-footer-original />
 </body>
