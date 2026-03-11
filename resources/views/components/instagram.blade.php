eebd1edaaba3ab3dc84c353ddb63f7b725215f6f
gregoriorondon <gregorioalexanderondon@gmail.com>
2026-03-10 20:45:38 -0400

    Feature: instagram post
    Dada las circunstancias con el componente npm para cargar el post de
    instagram que se había instalado y configurado, tarda demasiado para
    mostrar el contenido, sea que este en modo dev o ya se haya hecho el
    build del npm, incluso en ocasiones había que recargar un par de veces
    la pagina para que mostraran el contenido, cosa que es muy malo para la
    experiencia de los estudiantes y aspirantes.
    
    Por lo que se tomo la iniciativa de usar el propia de instagram que
    proporciona al copiar el enlace de inscrustación, pero modificandolo con
    ayuda de la IA para evitar perder tiempo eliminando secciones
    innecesarias.

---
+++ resources/views/components/instagram.blade.php
@@ -0,0 +1,7 @@
+@props(['url'])
+
+<blockquote class="instagram-media"
+    data-instgrm-permalink="{{ $url }}"
+    data-instgrm-version="14"
+    style="background:#FFF; border:0; border-radius:3px; margin:1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:calc(100% - 2px);">
+</blockquote>
